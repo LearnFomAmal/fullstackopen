dfc00178fdc23e24ef30b4a3a907fa0cd8c027f6
Amahhkkncfff <amalnair651@gmail.com>
2026-07-31 18:33:09 +0530

    completed unicafe

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -8,23 +8,53 @@ function App() {
       neutral:0,
       bad:0
   })
-const all=value.bad+value.good+value.neutral
+
+  const [clicked,setClicked]=useState(false);
+
   return (
 <div>
 <h1>give feedback</h1>
 
-  <button onClick={()=>setValue({...value,good:value.good+1})}>good</button>
-<button  onClick={()=>setValue({...value,neutral:value.neutral+1})}>neutral</button>
-<button  onClick={()=>setValue({...value,bad:value.bad+1})}>bad</button>
+  <button onClick={()=>{setValue({...value,good:value.good+1}); setClicked(true)}}>good</button>
+<button  onClick={()=>{setValue({...value,neutral:value.neutral+1});setClicked(true)}}>neutral</button>
+<button  onClick={()=>{setValue({...value,bad:value.bad+1});setClicked(true)}}>bad</button>
 
-<h1>statistics</h1>
- <p>good:{value.good}</p>
- <p>neutral:{value.neutral}</p>
- <p>bad:{value.bad}</p>
- <p>all:{all}</p>
-  <p>average:{all/3}</p>
+{clicked?<Statistics good={value.good} bad={value.bad} neutral={value.neutral}/>:<p>No feedback given</p>}
 </div>
   )
 }
+ const StaticLine=(props)=>{
 
+  return (
+    
+      <tbody>
+<tr>
+          <td>{props.text}</td>
+           <td>{props.value}</td>
+        </tr>
+      </tbody>
+            
+ 
+  )
+ }
+function Statistics(props){
+  const all=props.bad+props.good+props.neutral
+  return(
+  <div>
+    <h1>statistics</h1>
+<table>
+
+
+ <StaticLine text="good" value={props.good}/>
+  <StaticLine text="neutral" value={props.neutral} />
+   <StaticLine text="bad" value={props.bad}/>
+  <StaticLine text="all" value={all}/>
+   <StaticLine text="average" value={(props.good-props.bad)/all}/>
+    <StaticLine text="positive" value={(props.good/all)*100}/>
+
+</table>
+ 
+  </div>
+  )
+}
 export default App
