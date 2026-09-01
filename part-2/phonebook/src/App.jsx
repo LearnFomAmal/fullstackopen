4de4f923bf08a4274aa66c04121a5c62aef60ed2
LearnFromAmal <amalnair651@gmail.com>
2026-09-01 17:00:19 +0530

    phonebook till 2.11 is done

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
+import { useState,useEffect} from 'react'
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',number:'040-123456',id: 1 },
-    { name: 'Ada Lovelace',number:'39-44-5323523',id: 2 },
-    { name: 'Dan Abramov',number:'12-43-234345',id: 3 },
-    { name: 'Mary Poppendieck',number:'39-23-6423122',id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
 
   
   const [newName, setNewName] = useState('')
   const [number,setNumber]=useState('')
   const [filter,setFilter]=useState('')
+  const [loading,setLoading]=useState(true);
+  const baseUrl='http://localhost:3001/persons';
+
+  useEffect(()=>{
+    axios.get(baseUrl)
+    .then(response=>{
+      setPersons(response.data);
+      setLoading(false);
+    })
+  },[])
+
   const handleChange=(e)=>{
 
    console.log(e.target.value);
@@ -46,7 +53,7 @@ const App = () => {
     handleSubmit={handleSubmit} setNumber={setNumber}
     />
       <h3>Numbers</h3>
-     <Persons filteredPersons={filteredPersons}/>
+     <Persons filteredPersons={filteredPersons} loading={loading} />
     </div>
   )
 }
@@ -78,14 +85,15 @@ function PersonForm({newName,number,handleChange,handleSubmit,setNumber}){
     )
 }
 
-function Persons({filteredPersons}){
+function Persons({filteredPersons, loading}){
   return (
      <div>
-     {filteredPersons.map(person=>(
+      {loading ? <p>Loading...</p> : filteredPersons.map(person=>(
       <div key={person.id}>
     {person.name} - {person.number}
       </div>
      ))}
+    
     
         </div>
   )
